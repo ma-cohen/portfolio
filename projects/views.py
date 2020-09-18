8ef2a128b074902c364a15d8cdf89ef40e4fa63b
macohen <matan.workspace@gmail.com>
2020-09-18 17:32:19 +0300

    Add navbar and footer

---
+++ projects/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from projects.models import Project
+
+
+def all_projects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
