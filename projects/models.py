82d76b6c5b124d4dd43d705e3a48fe902b962ce3
macohen <matan.workspace@gmail.com>
2020-11-07 18:24:22 +0200

    change max length Project model

--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=50)
+    technology = models.CharField(max_length=60)
     image = models.CharField(max_length=100)
     github_link = models.URLField(default=None, max_length=250)
